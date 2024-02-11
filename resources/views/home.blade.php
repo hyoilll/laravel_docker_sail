f0fd662df621a14bf4815834d1d5d50b7f197226
hyoil Lee <dlgydlf123@gmail.com>
2024-02-11 22:21:45 +0900

    update: home 画面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,11 @@
-こんにちは！
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>トップ画面</title>
+</head>
+<body>
+    こんにちは！
+</body>
+</html>
