7e6f585862f17b9c81168c05226c136102b90562
hyoil Lee <dlgydlf123@gmail.com>
2024-02-20 00:00:02 +0900

    Factoryを利用 with Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(AuthorsTableSeeder::class);
+        // $this->call(AuthorsTableSeeder::class);
+
+        \App\Models\Publisher::factory(50)->create();
     }
 }
